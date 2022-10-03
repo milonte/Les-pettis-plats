5b89e24d3b8607fa55e94c600d78bba67ca7bee3
milonte <milonte@gmail.com>
2022-10-03 12:19:29 +0200

    feat: filter recipes result with search input

--- src/js/main.js
+++ src/js/main.js
@@ -9,21 +9,80 @@ import RecipeCard from './components/RecipeCard';
 const filtersResultsSections = document.getElementById("filters-results");
 const filtersButtonsSections = document.getElementById("filters-buttons");
 const recipesSection = document.getElementById("recipes-container");
+const recipeSearch = document.getElementById("recipe-search");
 const ingredients = [];
 const appliances = [];
 const ustensils = [];
-recipes.forEach(recipe => {
-    recipe.ingredients.forEach(ingredient => {
-        !ingredients.includes(ingredient.ingredient) ? ingredients.push(ingredient.ingredient) : null;
+function getRecipes() {
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(ingredient => {
+            !ingredients.includes(ingredient.ingredient) ? ingredients.push(ingredient.ingredient) : null;
+        });
+        !appliances.includes(recipe.appliance) ? appliances.push(recipe.appliance) : null;
+        recipe.ustensils.forEach(ustensil => {
+            !ustensils.includes(ustensil) ? ustensils.push(ustensil) : null;
+        });
     });
-    !appliances.includes(recipe.appliance) ? appliances.push(recipe.appliance) : null;
-    recipe.ustensils.forEach(ustensil => {
-        !ustensils.includes(ustensil) ? ustensils.push(ustensil) : null;
+    return recipes;
+}
+function displayRecipesCards(recipesFilters = null) {
+    const recipes = getRecipes();
+    const recipesToDisplay = [];
+    clearRecipesCard();
+    /* filter recipes if filters fund */
+    if (recipesFilters) {
+        for (const [key, value] of Object.entries(recipesFilters[0])) {
+            if ("search" == key) {
+                recipesToDisplay.push(recipes.filter(recipe => {
+                    if (
+                    /* if search value is in recipe name*/
+                    recipe.name.toLowerCase().includes(value.toLowerCase())
+                        /* or if search value is contains in recipe description */
+                        || recipe.description.toLowerCase().includes(value.toLowerCase())
+                        /* or if search value is contains in recpide ringredeints list */
+                        || recipe.ingredients.map(ingr => ingr.ingredient.toLowerCase().includes(value.toLowerCase())).includes(true)) {
+                        return recipe;
+                    }
+                }));
+            }
+        }
+    }
+    else {
+        recipesToDisplay.push(recipes);
+    }
+    if (recipesToDisplay[0].length <= 0) {
+        const noResultDiv = document.createElement("div");
+        noResultDiv.innerHTML = '« Aucune recette ne correspond à votre critère... vous pouvez chercher « tarte aux pommes », « poisson », etc';
+        recipesSection === null || recipesSection === void 0 ? void 0 : recipesSection.appendChild(noResultDiv);
+    }
+    /*
+        Display recipes cards
+    */
+    recipesToDisplay[0].forEach((recipe) => {
+        const recipeCard = new RecipeCard(recipe).getDOMElement();
+        recipesSection === null || recipesSection === void 0 ? void 0 : recipesSection.appendChild(recipeCard);
     });
-});
-recipes.forEach(recipe => {
-    const recipeCard = new RecipeCard(recipe).getDOMElement();
-    recipesSection === null || recipesSection === void 0 ? void 0 : recipesSection.appendChild(recipeCard);
+}
+function clearRecipesCard() {
+    if (recipesSection) {
+        recipesSection.innerHTML = "";
+    }
+}
+displayRecipesCards();
+/*
+    Search
+*/
+recipeSearch === null || recipeSearch === void 0 ? void 0 : recipeSearch.addEventListener('input', (e) => {
+    const searchValue = e.target.value;
+    if (searchValue.length > 2) {
+        displayRecipesCards([{
+                'search': searchValue,
+                'search2': searchValue
+            }]);
+    }
+    else {
+        displayRecipesCards();
+    }
 });
 /*
     Filters Buttons
--- src/js/main.ts
+++ src/js/main.ts
@@ -7,29 +7,105 @@ import { recipes } from '../../data/recipes';
 import KeyFilter from './components/KeyFilterButton';
 import KeyFilterPill from './components/KeyFilterPill';
 import RecipeCard from './components/RecipeCard';
+import { RecipeModel } from './model/RecipeModel';
 
 const filtersResultsSections: HTMLElement | null = document.getElementById("filters-results");
 const filtersButtonsSections: HTMLElement | null = document.getElementById("filters-buttons");
 const recipesSection: HTMLElement | null = document.getElementById("recipes-container");
 
+const recipeSearch: HTMLElement | null = document.getElementById("recipe-search");
+
 const ingredients : Array<string> = [];
 const appliances : Array<string> = [];
-const ustensils : Array<string> = [];
+const ustensils: Array<string> = [];
 
-recipes.forEach(recipe => { 
-    recipe.ingredients.forEach(ingredient => {
-        !ingredients.includes(ingredient.ingredient) ? ingredients.push(ingredient.ingredient) : null;
+function getRecipes() {
+
+    recipes.forEach(recipe => { 
+        recipe.ingredients.forEach(ingredient => {
+            !ingredients.includes(ingredient.ingredient) ? ingredients.push(ingredient.ingredient) : null;
+        });
+        !appliances.includes(recipe.appliance) ? appliances.push(recipe.appliance) : null;
+        recipe.ustensils.forEach(ustensil => {
+            !ustensils.includes(ustensil) ? ustensils.push(ustensil) : null;
+        });
     });
-    !appliances.includes(recipe.appliance) ? appliances.push(recipe.appliance) : null;
-    recipe.ustensils.forEach(ustensil => {
-        !ustensils.includes(ustensil) ? ustensils.push(ustensil) : null;
+
+    return recipes;
+}
+
+function displayRecipesCards(recipesFilters: Array<any> | null = null) {
+
+    const recipes = getRecipes();
+    const recipesToDisplay: Array<any> = [];
+
+    clearRecipesCard();
+        
+    /* filter recipes if filters fund */
+    if (recipesFilters) {
+
+        for (const [key, value] of Object.entries(recipesFilters[0])) {
+            if ("search" == key) {
+                recipesToDisplay.push(recipes.filter(recipe => {
+                    
+                    if (
+                        /* if search value is in recipe name*/
+                        recipe.name.toLowerCase().includes(value.toLowerCase())
+                        /* or if search value is contains in recipe description */
+                        || recipe.description.toLowerCase().includes(value.toLowerCase())
+                        /* or if search value is contains in recpide ringredeints list */
+                        || recipe.ingredients.map(ingr => ingr.ingredient.toLowerCase().includes(value.toLowerCase())).includes(true)
+                    ) {
+                        
+                        return recipe;
+                    }
+                }));
+            }
+        }
+    } else {
+        recipesToDisplay.push(recipes);
+    }
+
+    if (recipesToDisplay[0].length <= 0) {
+        const noResultDiv = document.createElement("div");
+        noResultDiv.innerHTML = '« Aucune recette ne correspond à votre critère... vous pouvez chercher « tarte aux pommes », « poisson », etc';
+        
+        recipesSection?.appendChild(noResultDiv);
+   }
+    /* 
+        Display recipes cards
+    */
+    recipesToDisplay[0].forEach((recipe: RecipeModel) => { 
+        const recipeCard = new RecipeCard(recipe).getDOMElement();
+    
+        recipesSection?.appendChild(recipeCard);
     });
-});
+}
 
-recipes.forEach(recipe => { 
-    const recipeCard = new RecipeCard(recipe).getDOMElement();
+function clearRecipesCard() {
+    if (recipesSection) {
+        recipesSection.innerHTML = "";
+    }
+}
 
-    recipesSection?.appendChild(recipeCard);
+displayRecipesCards();
+
+
+/* 
+    Search
+*/
+recipeSearch?.addEventListener('input', (e : any) => {
+    const searchValue: string = e.target.value;
+
+    if (searchValue.length > 2) {
+        displayRecipesCards([{
+            'search': searchValue,
+            'search2': searchValue
+        }]);
+    } else { 
+        displayRecipesCards();
+    }
+
 });
 
 /*
@@ -58,6 +134,4 @@ function drawPillCallback(type: string, value: string) {
 
         filtersResultsSections?.appendChild(pillButtonElement);
     }
-
-
 }
